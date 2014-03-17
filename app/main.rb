7525c25e40585c1a80b0851a99eac0518861a68a
Andy von Dohren <avondohren@gmail.com>
2014-03-17 17:20:58 -0500

    basic Term functionality

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,7 @@
 require 'sinatra/base'
+require 'active_record'
+require 'logger'
+require 'pry'
 require_relative './controllers/functions'
 
 class Sample < Sinatra::Base
@@ -10,5 +13,26 @@ class Sample < Sinatra::Base
   
   get "/" do
     erb :home
+  end
+  
+  get "/add/term" do
+    erb :add_term
+  end
+  
+  post "/add/term" do
+    name = params[:name]
+    new_task = Task.create({:name => name})
+    
+    redirect to("/term/#{new_term.id}")
+  end
+  
+  get "/list/term" do
+    @terms = Term.all
+    erb :list_term
+  end
+  
+  get "/term/:id" do
+    @term = Term.find(params[:id])
+    erb :term
   end
 end
